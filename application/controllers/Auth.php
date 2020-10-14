67a3b3acfa680f0edda426c63874a44d23065668
Dadan Hendriawan <dadan_hendriawan@yahoo.co.id>
2020-10-14 07:58:30 +0700

    melengkapi method registration pada controller auth, dan menambahkan form error pada view auth/registration dan flashdata pada view auth/login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,11 +6,12 @@ class Auth extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->library('form_validation');
     }
 
     public function index()
     {
-        $data['title'] = 'Login';
+        $data['title'] = 'Login Page';
         $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login', $data);
         $this->load->view('templates/auth_footer');
@@ -18,9 +19,33 @@ class Auth extends CI_Controller
 
     public function registration()
     {
-        $data['title'] = 'Registration';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/registration', $data);
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[tb_user.email]');
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[6]|matches[password2]', [
+            'min_length' => 'Password too short',
+            'matches' => 'Password dont matches'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        if ($this->form_validation->run() == FALSE) {
+            $data['title'] = 'User Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration', $data);
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'name' => $this->input->post('name', true),
+                'email' => $this->input->post('email', true),
+                'password' => password_hash($this->input->post('password1', true), PASSWORD_DEFAULT),
+                'image' => 'default.jpg',
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('tb_user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation your account has been<strong> Created</strong>, please login</div>');
+            redirect('auth');
+        }
     }
 }
