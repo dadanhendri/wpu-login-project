6278e525be58c3d37245dbaca7c3f3a56844400a
Dadan Hendriawan <dadan_hendriawan@yahoo.co.id>
2020-10-19 13:45:51 +0700

    membuat fungsi mencegah user yg sudah login masuk ke controller auth/logi atau auth/registration

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -11,6 +11,10 @@ class Auth extends CI_Controller
 
     public function index()
     {
+        if ($this->session->userdata('email')) {
+            redirect('user');
+        }
+
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
         $this->form_validation->set_rules('password', 'Password', 'required|trim|min_length[6]');
 
@@ -59,6 +63,10 @@ class Auth extends CI_Controller
 
     public function registration()
     {
+        if ($this->session->userdata('email')) {
+            redirect('user');
+        }
+
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[tb_user.email]');
         $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[6]|matches[password2]', [
