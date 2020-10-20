117b70f73c4da4b99f99ca90f8af65dd3c1e05fe
Dadan Hendriawan <dadan_hendriawan@yahoo.co.id>
2020-10-20 07:22:21 +0700

    menambahkan method edit profile pada controller user, menambahkan view user/edit, menambahkan fungsi pada java script di footer menambahkan nama file yg di upload pada input form

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -7,6 +7,8 @@ class User extends CI_Controller
     {
         parent::__construct();
         is_logged_in();
+        $this->load->helper('form');
+        $this->load->library('form_validation');
     }
 
 
@@ -19,5 +21,53 @@ class User extends CI_Controller
         $this->load->view('templates/topbar', $data);
         $this->load->view('user/index', $data);
         $this->load->view('templates/footer');
+    }
+
+    public function edit()
+    {
+        $data['title']  = 'Edit My Profile';
+        $data['user'] = $this->db->get_where('tb_user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $this->form_validation->set_rules('name', 'Full Name', 'required|trim');
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('user/edit', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $name = $this->input->post('name');
+            $email = $this->input->post('email');
+
+            $upload_file = $_FILES['image']['name'];
+
+            if ($upload_file) {
+                $config['allowed_types']        = 'gif|jpg|png';
+                $config['max_size']             = 2048;
+                $config['upload_path']          = './assets/img/profile';
+
+
+                $this->load->library('upload', $config);
+
+                if ($this->upload->do_upload('image')) {
+                    $old_image = $data['user']['image'];
+
+                    if ($old_image != 'default.jpg') {
+                        unlink(FCPATH . 'assets/img/profile/' . $old_image);
+                    }
+
+                    $new_image = $this->upload->data('file_name');
+                    $this->db->set('image', $new_image);
+                } else {
+                    echo $this->upload->display_errors();
+                }
+            }
+
+            $this->db->set('name', $name);
+            $this->db->where('email', $email);
+            $this->db->update('tb_user');
+            $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible fade show" role="alert"><strong>Your profile has been Updated</strong><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+            redirect('user');
+        }
     }
 }
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -124,6 +124,17 @@
                 }
             });
         });
+
+        $('.custom-file-input').on('change', function() {
+            let fileName = $(this).val().split('\\').pop();
+            $(this).next('.custom-file-label').addClass("selected").html(fileName);
+        });
+
+
+
+
+
+
     });
 </script>
 
